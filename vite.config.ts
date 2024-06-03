46054843ace165123223aed599b4c95b3d2320f3
Oleksandr Shepetko <a@shepetko.com>
2024-06-03 21:15:45 +0300

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
+	base: "/device",
 	build: {
 		outDir: 'build',
-		assetsDir: "device",
 	}
 });
