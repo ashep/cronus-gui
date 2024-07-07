b1e4773ea01c13b77d30176eb33c40c619e00a5d
Oleksandr Shepetko <a@shepetko.com>
2024-07-07 21:13:06 +0300

    Update

--- src/routes/Display/index.tsx
+++ src/routes/Display/index.tsx
@@ -6,11 +6,14 @@ import CssBaseline from "@mui/material/CssBaseline";
 import {Link, route} from "preact-router";
 
 import Stack from "@mui/material/Stack";
-import FormGroup from "@mui/material/FormGroup";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
+import Select from "@mui/material/Select";
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
@@ -21,6 +24,34 @@ interface Props {
 
 interface State {
 }
+
+const durationValues = [
+    {value: 0, label: "Don't show"},
+    {value: 5, label: '5 sec'},
+    {value: 15, label: '15 sec'},
+    {value: 30, label: '30 sec'},
+    {value: 45, label: '45 sec'},
+    {value: 60, label: '1 min'},
+];
+
+const brightnessValues = [
+    {value: 0, label: "1"},
+    {value: 1, label: "2"},
+    {value: 2, label: "3"},
+    {value: 3, label: "4"},
+    {value: 4, label: "5"},
+    {value: 5, label: "6"},
+    {value: 6, label: "7"},
+    {value: 7, label: "8"},
+    {value: 8, label: "9"},
+    {value: 9, label: "10"},
+    {value: 10, label: "11"},
+    {value: 11, label: "12"},
+    {value: 12, label: "13"},
+    {value: 13, label: "14"},
+    {value: 14, label: "15"},
+    {value: 15, label: "16"},
+]
 
 export default class Display extends React.Component<Props, State> {
     render(): React.JSX.Element {
@@ -40,43 +71,95 @@ export default class Display extends React.Component<Props, State> {
                     Click <Link href={"/device/"}>here</Link> to connect.
                 </Box>
                 :
-                <Stack direction={"column"} spacing={1}>
-                    <FormGroup>
-                        {this.props.cfgSvc.DisplayType == ConfigSvc.DisplayType.MAX7219_32X16 &&
-                            <FormControlLabel
-                                label="Two-line mode"
-                                control={
-                                    <Checkbox checked={this.props.cfgSvc.MultilineMode}
-                                              onChange={(_, v) => this.props.cfgSvc.SetMultilineMode(v)}
-                                    />
-                                }
+                <Stack direction={"column"} spacing={3}>
+                    <FormControlLabel
+                        label="Multiline mode"
+                        control={
+                            <Checkbox checked={this.props.cfgSvc.MultilineMode}
+                                      onChange={(_, v) => this.props.cfgSvc.SetMultilineMode(v)}
                             />
                         }
-                        <FormControlLabel
-                            label="Show date"
-                            control={
-                                <Checkbox checked={this.props.cfgSvc.ShowDate}
-                                          onChange={(_, v) => this.props.cfgSvc.SetShowDate(v)}
-                                />
-                            }
-                        />
-                        <FormControlLabel
-                            label="Show ambient temperature"
-                            control={
-                                <Checkbox checked={this.props.cfgSvc.ShowAmbientTemp}
-                                          onChange={(_, v) => this.props.cfgSvc.SetShowAmbientTemp(v)}
-                                />
-                            }
-                        />
-                        <FormControlLabel
-                            label="Show weather temperature"
-                            control={
-                                <Checkbox checked={this.props.cfgSvc.ShowWeatherTemp}
-                                          onChange={(_, v) => this.props.cfgSvc.SetShowWeatherTemp(v)}
-                                />
-                            }
-                        />
-                    </FormGroup>
+                    />
+
+                    <FormControl variant="standard">
+                        <InputLabel id="min-brightness-label">Minimum brightness</InputLabel>
+                        <Select
+                            labelId="min-brightness-duration"
+                            id="min-brightness-duration-select"
+                            value={this.props.cfgSvc.DisplayMinBrightness}
+                            label="minimum brightness level"
+                            onChange={(_, v) => this.props.cfgSvc.SetDisplayMinBrightness(v.props.value)}
+                        >
+                            {brightnessValues.map((opt) => {
+                                    if (opt.value <= this.props.cfgSvc.DisplayMaxBrightness) {
+                                        return <MenuItem key={opt.value} value={opt.value}>{opt.label}</MenuItem>
+                                    }
+                                }
+                            )}
+                        </Select>
+                    </FormControl>
+
+                    <FormControl variant="standard">
+                        <InputLabel id="max-brightness-label">Maximum brightness</InputLabel>
+                        <Select
+                            labelId="max-brightness-duration"
+                            id="max-brightness-duration-select"
+                            value={this.props.cfgSvc.DisplayMaxBrightness}
+                            label="Maximum brightness level"
+                            onChange={(_, v) => this.props.cfgSvc.SetDisplayMaxBrightness(v.props.value)}
+                        >
+                            {brightnessValues.map(opt => (
+                                <MenuItem key={opt.value} value={opt.value}>{opt.label}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+
+                    {!this.props.cfgSvc.MultilineMode && (
+                        <FormControl variant="standard">
+                            <InputLabel id="show-time-duration-label">Show time</InputLabel>
+                            <Select
+                                labelId="show-time-duration"
+                                id="show-time-duration-select"
+                                value={this.props.cfgSvc.ShowTimeDuration}
+                                label="Show time"
+                                onChange={(_, v) => this.props.cfgSvc.SetShowTimeDuration(v.props.value)}
+                            >
+                                {durationValues.map(opt => (
+                                    <MenuItem key={opt.value} value={opt.value}>{opt.label}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    )}
+
+                    <FormControl variant="standard">
+                        <InputLabel id="show-date-duration-label">Show date</InputLabel>
+                        <Select
+                            labelId="show-time-duration"
+                            id="show-time-duration-select"
+                            value={this.props.cfgSvc.ShowDateDuration}
+                            label="Show time"
+                            onChange={(_, v) => this.props.cfgSvc.SetShowDateDuration(v.props.value)}
+                        >
+                            {durationValues.map(opt => (
+                                <MenuItem key={opt.value} value={opt.value}>{opt.label}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+
+                    <FormControl variant="standard">
+                        <InputLabel id="show-odr-temp-duration-label">Show outdoor temp</InputLabel>
+                        <Select
+                            labelId="show-odr-temp-duration"
+                            id="show-odr-duration-select"
+                            value={this.props.cfgSvc.ShowOdrTempDuration}
+                            label="Show outdoor temperature"
+                            onChange={(_, v) => this.props.cfgSvc.SetShowOdrTempDuration(v.props.value)}
+                        >
+                            {durationValues.map(opt => (
+                                <MenuItem key={opt.value} value={opt.value}>{opt.label}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
                     <Button variant={"outlined"} startIcon={<ArrowBackIcon/>} onClick={() => route("/device")}>
                         Back
                     </Button>
--- src/service/Config.ts
+++ src/service/Config.ts
@@ -20,8 +20,17 @@ enum cfgID {
     versionMajor = 0,
     versionMinor = 1,
     versionPatch = 2,
-    displayType = 3,
-    settings1 = 4,
+    flags1 = 3,
+
+    displayType = 10,
+    displayMinBrightness = 11,
+    displayMaxBrightness = 12,
+
+    showTimeDuration = 20,
+    showDateDuration = 21,
+    showDOWDuration = 22,
+    showAmbTempDuration = 23,
+    showOdrTempDuration = 24,
 }
 
 enum cfgBitsSettings1 {
@@ -35,13 +44,18 @@ export class Service {
     private btSvc: btSvc;
     private fwVer: Signal<fwVer> = signal({Major: 0, Minor: 0, Patch: 0});
     private displayType: Signal<DisplayType> = signal(DisplayType.NONE);
+    private displayMinBrightness: Signal<number> = signal(0);
+    private displayMaxBrightness: Signal<number> = signal(15);
 
     private multilineMode: Signal<boolean> = signal(false);
-    private showDate: Signal<boolean> = signal(false);
-    private showAmbientTemp: Signal<boolean> = signal(false);
-    private showWeatherTemp: Signal<boolean> = signal(false);
 
-    private data: DataView = new DataView(new ArrayBuffer(16));
+    private showTimeDuration: Signal<number> = signal(30);
+    private showDateDuration: Signal<number> = signal(5);
+    private showDOWDuration: Signal<number> = signal(5);
+    private showAmbTempDuration: Signal<number> = signal(5);
+    private showOdrTempDuration: Signal<number> = signal(5);
+
+    private data: DataView = new DataView(new ArrayBuffer(64)); // must be the same as firmware's CRONUS_CFG_CFG_BUF_LEN
 
     constructor(btSvc: btSvc) {
         this.btSvc = btSvc;
@@ -56,20 +70,81 @@ export class Service {
         return this.displayType.value;
     }
 
+    get DisplayMinBrightness(): number {
+        return this.displayMinBrightness.value;
+    }
+
+    get DisplayMaxBrightness(): number {
+        return this.displayMaxBrightness.value;
+    }
+
     get MultilineMode(): boolean {
         return this.multilineMode.value
     }
 
-    get ShowDate(): boolean {
-        return this.showDate.value;
+    get ShowTimeDuration(): number {
+        return this.showTimeDuration.value;
     }
 
-    get ShowAmbientTemp(): boolean {
-        return this.showAmbientTemp.value;
+    get ShowDateDuration(): number {
+        return this.showDateDuration.value;
     }
 
-    get ShowWeatherTemp(): boolean {
-        return this.showWeatherTemp.value;
+    get ShowOdrTempDuration(): number {
+        return this.showOdrTempDuration.value;
+    }
+
+    async SetDisplayMinBrightness(v: number) {
+        this.displayMinBrightness.value = v;
+        this.data.setInt8(cfgID.displayMinBrightness, v);
+        return this.btSvc.write(chcUUID, this.data);
+    }
+
+    async SetDisplayMaxBrightness(v: number) {
+        this.displayMaxBrightness.value = v;
+        this.data.setInt8(cfgID.displayMaxBrightness, v);
+        return this.btSvc.write(chcUUID, this.data);
+    }
+
+    async SetMultilineMode(v: boolean): Promise<void> {
+        this.multilineMode.value = v;
+
+        this.data.setInt8(
+            cfgID.flags1,
+            this.setBit(this.data.getInt8(cfgID.flags1), cfgBitsSettings1.multilineMode, v)
+        );
+
+        return this.btSvc.write(chcUUID, this.data);
+    }
+
+    async SetShowTimeDuration(v: number): Promise<void> {
+        this.showTimeDuration.value = v;
+        this.data.setInt8(cfgID.showTimeDuration, v);
+        return this.btSvc.write(chcUUID, this.data);
+    }
+
+    async SetShowDateDuration(v: number): Promise<void> {
+        this.showDateDuration.value = v;
+        this.data.setInt8(cfgID.showDateDuration, v);
+        return this.btSvc.write(chcUUID, this.data);
+    }
+
+    async SetShowDOWDuration(v: number): Promise<void> {
+        this.showDOWDuration.value = v;
+        this.data.setInt8(cfgID.showDOWDuration, v);
+        return this.btSvc.write(chcUUID, this.data);
+    }
+
+    async SetShowAmbTempDuration(v: number): Promise<void> {
+        this.showAmbTempDuration.value = v;
+        this.data.setInt8(cfgID.showAmbTempDuration, v);
+        return this.btSvc.write(chcUUID, this.data);
+    }
+
+    async SetShowOdrTempDuration(v: number): Promise<void> {
+        this.showOdrTempDuration.value = v;
+        this.data.setInt8(cfgID.showOdrTempDuration, v);
+        return this.btSvc.write(chcUUID, this.data);
     }
 
     private async fetch() {
@@ -86,55 +161,15 @@ export class Service {
         };
 
         this.displayType.value = this.data.getInt8(cfgID.displayType);
-        this.multilineMode.value = Boolean(this.data.getInt8(cfgID.settings1) & 1 << cfgBitsSettings1.multilineMode);
-        this.showDate.value = Boolean(this.data.getInt8(cfgID.settings1) & 1 << cfgBitsSettings1.showDate);
-        this.showAmbientTemp.value = Boolean(this.data.getInt8(cfgID.settings1) & 1 << cfgBitsSettings1.showAmbientTemp);
-        this.showWeatherTemp.value = Boolean(this.data.getInt8(cfgID.settings1) & 1 << cfgBitsSettings1.showWeatherTemp);
+        this.multilineMode.value = Boolean(this.data.getInt8(cfgID.flags1) & 1 << cfgBitsSettings1.multilineMode);
+
+        this.showTimeDuration.value = this.data.getInt8(cfgID.showTimeDuration);
+        this.showDateDuration.value = this.data.getInt8(cfgID.showDateDuration);
+        this.showDOWDuration.value = this.data.getInt8(cfgID.showDOWDuration);
+        this.showAmbTempDuration.value = this.data.getInt8(cfgID.showAmbTempDuration);
+        this.showOdrTempDuration.value = this.data.getInt8(cfgID.showOdrTempDuration);
     }
 
-    async SetMultilineMode(v: boolean): Promise<void> {
-        this.multilineMode.value = v;
-
-        this.data.setInt8(
-            cfgID.settings1,
-            this.setBit(this.data.getInt8(cfgID.settings1), cfgBitsSettings1.multilineMode, v)
-        );
-
-        return this.btSvc.write(chcUUID, this.data);
-    }
-
-    async SetShowDate(v: boolean): Promise<void> {
-        this.showDate.value = v;
-
-        this.data.setInt8(
-            cfgID.settings1,
-            this.setBit(this.data.getInt8(cfgID.settings1), cfgBitsSettings1.showDate, v)
-        );
-
-        return this.btSvc.write(chcUUID, this.data);
-    }
-
-    async SetShowAmbientTemp(v: boolean): Promise<void> {
-        this.showAmbientTemp.value = v;
-
-        this.data.setInt8(
-            cfgID.settings1,
-            this.setBit(this.data.getInt8(cfgID.settings1), cfgBitsSettings1.showAmbientTemp, v)
-        );
-
-        return this.btSvc.write(chcUUID, this.data);
-    }
-
-    async SetShowWeatherTemp(v: boolean): Promise<void> {
-        this.showWeatherTemp.value = v;
-
-        this.data.setInt8(
-            cfgID.settings1,
-            this.setBit(this.data.getInt8(cfgID.settings1), cfgBitsSettings1.showWeatherTemp, v)
-        );
-
-        return this.btSvc.write(chcUUID, this.data);
-    }
 
     private setBit(byte: number, pos: number, v: boolean): number {
         if (v) {
